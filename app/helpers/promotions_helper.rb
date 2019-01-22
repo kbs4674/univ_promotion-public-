9f4556bc451c2fab066522e15923c652a8066379
ChulMin Kim <kbs4674@likelion.org>
2019-01-22 08:08:04 +0000

    한자 출력이 안되던 문제 해결(글꼴을 바꿈.)

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -1,5 +1,5 @@
 module PromotionsHelper
     def render_with_hashtags(univ_hashtag)
-        univ_hashtag.gsub(/[#＃][a-z|A-Z|가-힣|0-9|\w]+/){|word| link_to word, "/promotions/hashtag/#{word.delete("#")}"}.html_safe
+        univ_hashtag.gsub(/[#＃][a-z|A-Z|가-힣|0-9|樂|\w]+/){|word| link_to word, "/promotions/hashtag/#{word.delete("#")}", style: "font-family: 'Dovemayo-Bold'"}.html_safe
     end
 end
