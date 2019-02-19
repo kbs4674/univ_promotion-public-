fee5620710db04890e81125fce721e826c4cc941
ChulMin Kim <kbs4674@likelion.org>
2019-02-19 16:54:16 +0000

    Email 기능 테스트 완료, 도입

--- app/mailers/sendmail_mailer.rb
+++ app/mailers/sendmail_mailer.rb
@@ -3,7 +3,7 @@ class SendmailMailer < ApplicationMailer
     @message = message
     mail from: 'kbs4674@damda.info',
         to: "#{@message.conversation.recipient.email}",
-        subject: "#{@message.conversation.sender.nickname} 님으로부터 메세지가 도착했습니다.",
+        subject: "[담다] #{@message.conversation.sender.nickname} 님으로부터 메세지가 도착했습니다.",
         html: render(template: "sendmail_mailer/message_notification")
     end
 end
